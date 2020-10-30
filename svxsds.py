e3e33bf10ed2b86c0cd30db340e5fbce53401515
Ronald PE2KMV <pe2kmv@gmail.com>
2020-10-30 13:25:07 +0100

    Daemon attempt

--- svxsds.py
+++ svxsds.py
@@ -1,7 +1,9 @@
 # dependencies
+import sys
 import time
 import serial
 import subprocess
+from functions import ProcessStatus, ScreenSDS, ProcessSDS
 from statuslist import status_table as status
 
 # settings
@@ -17,36 +19,51 @@ ser.rtscts = False
 ser.dsrdtr = False
 ser.writeTimeout = 2
 
+pidi = "/tmp/svxsds.pid"
+
 if ser.isOpen:
 	ser.close()
 
-def ProcessSDS (rawsds):
-	rawsds = rawsds.split("\\r\\n")
-	print(rawsds[2])
-	prep_echo = 'echo "' + status[rawsds[2]] + '" > /tmp/pty_ctl'
-	print(prep_echo)
-	subprocess.call(prep_echo, shell=True)
-	return()
-
 # open serial port
-try:
-	ser.open()
-except:
-	print("Error opening serial port")
-	exit()
-
-if ser.isOpen:
-
+def OpenSerialPort():
 	try:
-		ser.flushInput()
-		ser.flushOutput()
-		while True:
-			response = ser.readall()
-			if len(response) >0:
-				response = str(response)
-				ProcessSDS(response)
-
+		ser.open()
 	except:
-		ser.close()
-		print("script error")
-		exit(0)
+		print("Error opening serial port")
+		exit()
+
+def main():
+	if ser.isOpen:
+
+		try:
+			ser.flushInput()
+			ser.flushOutput()
+			while True:
+				response = ser.readall()
+				ser.flushInput()
+				ser.flushOutput()
+				if len(response) >0:
+					response = str(response)
+					MessageType = ScreenSDS(response)
+					if MessageType == "13":
+						svxmessage = ProcessStatus(response)
+						print(MessageType + " - " + svxmessage)
+						subprocess.call(svxmessage, shell=True)
+					if MessageType == "12":
+						svxmessage = ProcessSDS(response)
+						print(MessageType + " - " + svxmessage)
+						subprocess.call(svxmessage, shell=True)
+
+		except:
+			ser.close()
+			print("script error")
+			exit(0)
+
+
+if __name__ == '__main__':
+	try:
+		OpenSerialPort()
+		main()
+	except KeyboardInterrupt:
+		print >> sys.stderr, '\nExiting by user request\n'
+		sys.exit(0)
