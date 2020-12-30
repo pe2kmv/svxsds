f9d23261cc8b59c4cbe0ba6200ff742138f87b14
Ronald PE2KMV <pe2kmv@gmail.com>
2020-12-30 09:09:51 +0100

    Added aprs.fi upload

---
+++ aprs_function.py
@@ -0,0 +1,58 @@
+# load necessary libraries
+import aprslib
+import logging
+import time
+import configparser
+from datetime import datetime
+from aprslib.util import latitude_to_ddm, longitude_to_ddm, comment_altitude
+
+logger = logging.getLogger(__name__)
+
+# set variables
+config = configparser.ConfigParser()
+config.read('/home/pi/svx_sds/config.cfg')
+aprs_user = config.get('aprs','aprsuser')
+aprs_pw = config.get('aprs','aprspw')
+
+# connect to APRS_IS
+AIS = aprslib.IS(aprs_user,passwd=aprs_pw,port=14580)
+
+# get timestamp for APRS
+timestamp = datetime.utcfromtimestamp(time.time()).strftime("%d%H%M") + 'z'
+
+def SendAPRS(tempCall, tempLat, tempLong):
+	# create floats
+	tempLat = float(tempLat)
+	tempLong = float(tempLong)
+	if tempCall != None and tempLat != 0 and tempLong !=0:
+		# get timestamp for APRS
+		timestamp = datetime.utcfromtimestamp(time.time()).strftime("%d%H%M") + 'z'
+		APRSString = tempCall + '>APRS,TCPIP*:@' + timestamp + latitude_to_ddm(tempLat) + '/' + longitude_to_ddm(tempLong) + 'fTetra APRS station'
+		logger.debug('aprs string = ' + APRSString)
+		try:
+			AIS.connect()
+			AIS.sendall(APRSString)
+			logger.debug('SendAPRS - APRS string sent - '+ APRSString)
+			return()
+		except:
+			logger.error('SendAPRS - Failed to send APRS string')
+			return()
+
+def GetAPRSLat(tempLat):
+	if float(tempLat) > 0:
+		NS = 'N'
+	if float(tempLat) < 0:
+		NS = 'S'
+	tempLat = str(tempLat)
+	tempLat = tempLat.split('.')
+	aprsLat = tempLat[0].zfill(2) + tempLat[1][0:2] + '.'+ tempLat[1][2:4] + NS
+	return(aprsLat)
+
+def GetAPRSLong(tempLong):
+	if float(tempLong) > 0:
+		EW = 'E'
+	if float(tempLong) < 0:
+		EW = 'W'
+	tempLong = str(tempLong).split('.')
+	aprsLong = tempLong[0].zfill(3) + tempLong[1][0:2] + '.' + tempLong[1][2:4] + EW
+	return(aprsLong)
