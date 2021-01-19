715c6d04ccdbfc62752a9a6883761f1dad2d408f
Ronald PE2KMV <pe2kmv@gmail.com>
2021-01-19 17:29:46 +0100

    Serial port and speed via config file

--- initradio.py
+++ initradio.py
@@ -1,16 +1,23 @@
 import serial
 import time
 import logging
+import configparser
 
 logger = logging.getLogger(__name__)
 
 serialwait = 15 # Serial command timeout in seconds
 atcommands = ['AT&F','AT+CTOM=6','AT+CTSP=1,3,131','AT+CTSP=1,3,130','AT+CTSP=2,0,0','AT+CTSP=1,2,20','AT+CTSP=1,2,24','AT+CTSP=1,2,25','AT+CTSP=1,1,11','AT+CTSP=1,3,137']
 
+# get configuration
+config = configparser.ConfigParser()
+config.read('/etc/svxsds.cfg')
+ser_port = config.get('serial','port')
+ser_speed = int(config.get('serial','speed'))
+
 # settings
 ser = serial.Serial()
-ser.port = '/dev/ttyUSB0'
-ser.baudrade = 9600
+ser.port = ser_port
+ser.baudrade = ser_speed
 ser.bytesize = serial.EIGHTBITS
 ser.parity = serial.PARITY_NONE
 ser.stopbits = serial.STOPBITS_ONE
