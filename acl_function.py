144e54baea1758912f884398e41782c5b2d3862c
Ronald PE2KMV <pe2kmv@gmail.com>
2021-01-23 23:52:08 +0100

    Added function to retrieve APRS symbol and payload text from DB

--- acl_function.py
+++ acl_function.py
@@ -26,7 +26,7 @@ def ACL_InitDB():
 		mydb.close()
 		return()
 	if result == 0:
-		createstring = 'CREATE TABLE IF NOT EXISTS ' + db_table + '(acl_ID INT NOT NULL AUTO_INCREMENT,acl_ISSI INT NOT NULL,acl_call VARCHAR(10) NULL,acl_name VARCHAR(20) NULL,PRIMARY KEY (acl_ID))'
+		createstring = 'CREATE TABLE IF NOT EXISTS ' + db_table + '(acl_ID INT NOT NULL AUTO_INCREMENT,acl_ISSI INT NOT NULL,acl_call VARCHAR(10) NULL,acl_passwd VARCHAR(254),acl_name VARCHAR(20) NULL,acl_aprs_table VARCHAR(1),acl_aprs_symbol VARCHAR(1),acl_aprs_text VARCHAR(36),acl_auth_user BOOLEAN NOT NULL DEFAULT FALSE,PRIMARY KEY (acl_ID))'
 		dbcursor.execute(createstring)
 		logger.info('Created new table')
 		dbcursor.close()
@@ -37,10 +37,43 @@ def ACL_InitDB():
 def IsInACL(tempISSI):
 	mydb = MySQLdb.connect(host=db_host,user=db_user,passwd=db_pw,db=db_database)
 	dbcursor = mydb.cursor()
-	querystring = 'SELECT * FROM `' + db_table + '` WHERE `acl_ISSI` = ' + tempISSI
+	querystring = 'SELECT acl_auth_user FROM `' + db_table + '` WHERE `acl_ISSI` = ' + tempISSI
 	result = dbcursor.execute(querystring)
 	if result == 1:
-		return(True)
+		if dbcursor.fetchone()[0] == 1:
+			return(True)
+		else:
+			return(False)
 	if result == 0:
 		return(False)
 
+def GetAPRSSymbol(tempINPUT,inputtype):
+	mydb = MySQLdb.connect(host=db_host,user=db_user,passwd=db_pw,db=db_database)
+	dbcursor = mydb.cursor()
+	if inputtype.upper() == "ISSI":
+		querystring = 'SELECT CONCAT (acl_aprs_table, acl_aprs_symbol) AS fullsymbol FROM `' + db_table + '` WHERE `acl_ISSI` = ' + tempINPUT
+	if inputtype.upper() == "CALL":
+		querystring = 'SELECT CONCAT (acl_aprs_table, acl_aprs_symbol) AS fullsymbol FROM `' + db_table + '` WHERE `acl_call` = "' + tempINPUT + '"'
+
+	try:
+		result = dbcursor.execute(querystring)
+		return(dbcursor.fetchone()[0])
+	except:
+		return('TA')
+
+def GetAPRSText(tempINPUT,inputtype):
+	mydb = MySQLdb.connect(host=db_host,user=db_user,passwd=db_pw,db=db_database)
+	dbcursor = mydb.cursor()
+	if inputtype.upper() == "ISSI":
+		querystring = 'SELECT acl_aprs_text FROM `' + db_table + '` WHERE acl_ISSI = ' + tempINPUT
+	if inputtype.upper() == "CALL":
+		querystring = 'SELECT acl_aprs_text FROM `' + db_table + '` WHERE acl_call = "' + tempINPUT + '"'
+	try:
+		result = dbcursor.execute(querystring)
+		temptext = dbcursor.fetchone()[0]
+		if temptext == '':
+			return('TetrAPRS station')
+		else:
+			return(temptext)
+	except:
+		return('TetrAPRS station')
