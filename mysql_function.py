c3852cd60321e3db5dc674281aef2e6affc63348
Ronald PE2KMV <pe2kmv@gmail.com>
2021-02-12 22:14:36 +0100

    Added function to change APRS beacon text for known ISSI

--- mysql_function.py
+++ mysql_function.py
@@ -4,6 +4,7 @@ import logging
 import configparser
 
 from newlip import *
+from acl_function import IsInACL
 
 logger = logging.getLogger(__name__)
 
@@ -17,6 +18,7 @@ tetraprs_pw = config.get('mysql','db_pw')
 tetraprs_host = config.get('mysql','db_host')
 tetraprs_db = config.get('mysql','db_database')
 tetraprs_table = config.get('mysql','db_table')
+tetraprs_acl = config.get('mysql','db_acl')
 
 def add_to_db(TempTimeStamp,TempISSI, TempCallSign,TempSDS):
 	try:
@@ -53,3 +55,20 @@ def InitDB():
 		mydb.close()
 		return()
 
+# Set a new APRS beacon text in the database
+def MySQL_SetText(tempissi,temptext):
+	try:
+		if (IsInACL(tempissi)) == True:
+			db = MySQLdb.connect(host=tetraprs_host,user=tetraprs_user,passwd=tetraprs_pw,db=tetraprs_db)
+			cur = db.cursor()
+			cur.execute('UPDATE ' + tetraprs_acl + ' SET acl_aprs_text = "' + temptext + '" WHERE acl_ISSI = ' + tempissi)
+			db.commit()
+			cur.close()
+			db.close()
+			logger.debug('Updated APRS beacon text for ISSI ' + tempissi + ' to ' + temptext)
+		else:
+			logger.debug('ISSI not known in ACL')
+		return()
+	except:
+		logger.debug('Could not update APRS beacon text')
+		return()
