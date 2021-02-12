7a252248055c8d0968ba6c12a094859f18d87e09
Ronald PE2KMV <pe2kmv@gmail.com>
2021-02-12 22:13:37 +0100

    Added function to determine APRS command

--- aprs_function.py
+++ aprs_function.py
@@ -6,6 +6,7 @@ import configparser
 from datetime import datetime
 from aprslib.util import latitude_to_ddm, longitude_to_ddm, comment_altitude
 from acl_function import GetAPRSSymbol,GetAPRSText
+from mysql_function import MySQL_SetText
 
 logger = logging.getLogger(__name__)
 
@@ -21,6 +22,18 @@ AIS = aprslib.IS(aprs_user,passwd=aprs_pw,port=14580)
 
 # get timestamp for APRS
 timestamp = datetime.utcfromtimestamp(time.time()).strftime("%d%H%M") + 'z'
+
+# this command changes APRS settings
+def AprsCommand(tempissi,rawsds):
+	try:
+		tempcmd = rawsds.split(' ',1)[0]
+		if tempcmd == 'settext':
+			# this sets a new APRS beacon text in the database
+			MySQL_SetText(tempissi,rawsds.split(' ',1)[1])
+		else:
+			return()
+	except:
+		return()
 
 def SendAPRS(tempCall, tempLat, tempLong,tempPayLoad):
 	if tetraprs_useaprs != "True":
