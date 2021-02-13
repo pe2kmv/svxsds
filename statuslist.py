22d8ecbcb97be397c9d85f5d726ab64ad4d077e6
Ronald PE2KMV <pe2kmv@gmail.com>
2021-02-13 19:13:36 +0100

    Added MIC-E position comments

--- statuslist.py
+++ statuslist.py
@@ -10,5 +10,13 @@ status_table = {
 	"9009": "9#",
 	"900A": "0#",
 	"900B": "*#",
-	"900C": "#"
+	"900C": "#",
+	'8001': 'Off Duty',
+	'8002': 'En Route',
+	'8003': 'In Service',
+	'8004': 'Returning',
+	'8005': 'Committed',
+	'8006': 'Special',
+	'8007': 'Priority',
+	'8008': 'Emergency'
 }
