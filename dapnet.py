e878c00ed694cf8ffcdf6c0ed851cb5c2e0b5a3c
Ronald PE2KMV <pe2kmv@gmail.com>
2021-04-04 22:42:43 +0200

    Changed logging

--- dapnet.py
+++ dapnet.py
@@ -1,8 +1,7 @@
-import logging
+from logfunctions import *
 import configparser
 import requests
 import json
-logger = logging.getLogger(__name__)
 
 # set variables
 config = configparser.ConfigParser()
@@ -14,9 +13,10 @@ dapnet_uri = config.get('dapnet','dapneturi')
 def SendDapnet(tempsds):
 	tempsds = tempsds.strip()
 	tempsds = tempsds.split(' ',1)
-	send_page(tempsds[0],3,'all','false',tempsds[1])
+	send_page(tempsds[0].lower(),3,'all','false',tempsds[1])
 
 def send_page(msg_ric,func,trx,emr,msgtxt):
+	dapnet_logger.info(msg_ric + '|' + msgtxt)
 	req = requests.post(dapnet_uri,auth=(dapnet_user,dapnet_pw),json={'text':msgtxt,'callSignNames':[msg_ric],'transmitterGroupNames':[trx],'emergency':emr})
-	logging.debug(req)
+	dapnet_logger.debug(req)
 
