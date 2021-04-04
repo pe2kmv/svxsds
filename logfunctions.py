9595c2762062036a3fd57a462f1b740b7fa2de00
Ronald PE2KMV <pe2kmv@gmail.com>
2021-04-04 22:40:45 +0200

    Added extended logfunctions

---
+++ logfunctions.py
@@ -0,0 +1,22 @@
+import logging
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+
+
+def setup_logger(name, log_file, level=logging.DEBUG):
+	handler = logging.FileHandler(log_file)
+	handler.setFormatter(formatter)
+
+	logger = logging.getLogger(name)
+	logger.setLevel(level)
+	logger.addHandler(handler)
+
+	return(logger)
+
+# main logger
+logger = setup_logger('main_logger', '/var/log/svxsds.log')
+
+# dapnet logger
+dapnet_logger = setup_logger('dapnet_logger', '/var/log/sdsdapnet.log')
+
+# aprs logger
+aprs_logger = setup_logger('aprs_logger','/var/log/sdsaprs.log')
